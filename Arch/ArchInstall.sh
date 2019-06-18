51c01369bee95714da8b479b842e1f57b8afa335
nobabykill <linh1612340@gmail.com>
2019-06-18 17:07:51 +0700

    update file auto install arch script

--- Arch/ArchInstall.sh
+++ Arch/ArchInstall.sh
@@ -10,7 +10,7 @@ read -p 'input user password : ' userpass
 
 ## copy file config mirror list
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup # backup mirror
-cp /configure/Arch/mirrorlist /etc/pacman.d/mirrorlist
+cp /configure/Arch/config_file/mirrorlist /etc/pacman.d/mirrorlist
 pacman -Syyu
 
 ## install intel-ucode
@@ -31,16 +31,8 @@ echo "ArchLinux" > /etc/hostname
 ## install boot loader
 bootctl --path=/boot install
 
-rm -rf /boot/loader/loader.conf
-echo 'default arch' >> /boot/loader/loader.conf
-echo 'timeout 0' >> /boot/loader/loader.conf
-echo 'editor 0' >> /boot/loader/loader.conf
-
-echo 'title Arch Linux' >> /boot/loader/entries/arch.conf
-echo 'linux /vmlinuz-linux' >> /boot/loader/entries/arch.conf
-echo 'initrd /intel-ucode.img' >> /boot/loader/entries/arch.conf
-echo 'initrd /initramfs-linux.img' >> /boot/loader/entries/arch.conf
-echo 'options root=/dev/sda2 rw' >> /boot/loader/entries/arch.conf
+cp /configure/Arch/config_file/loader.conf /boot/loader/loader.conf
+cp /configure/Arch/config_file/arch.conf /boot/loader/entries/arch.conf
 
 ## set root password
 echo "root:$rootpass" | chpasswd
@@ -59,21 +51,21 @@ echo 'exec i3' >> ~/.xinitrc
 sudo pacman -S --noconfirm i3-wm i3blocks
 mkdir -p ~/.config/i3
 mkdir ~/.config/i3blocks
-cp /configure/Arch/i3.config ~/.config/i3/config
-cp /configure/Arch/i3blocks.conf ~/.config/i3blocks/config
+cp /configure/Arch/config_file/i3 ~/.config/i3/config
+cp /configure/Arch/config_file/i3blocks ~/.config/i3blocks/config
 sudo pacman -S --noconfirm sysstat
 
 ## install urxvt
 sudo pacman -S --noconfirm rxvt-unicode
-cp /configure/Arch/Xresources ~/.Xresources
+cp /configure/Arch/config_file/Xresources ~/.Xresources
 
 ## install gvim
 sudo pacman -S --noconfirm gvim
-cp /configure/Arch/vimrc ~/.vimrc
+cp /configure/Arch/config_file/vimrc ~/.vimrc
 
 ## install zsh
 sudo pacman -S --noconfirm zsh zsh-completions dash
-cp /configure/Arch/zshrc ~/.zshrc # copy config 
+cp /configure/Arch/config_file/zshrc ~/.zshrc # copy config 
 
 ## audio
 sudo pacman -S --noconfirm alsa-utils pulseaudio-alsa pamixer
@@ -82,7 +74,7 @@ sudo pacman -S --noconfirm alsa-utils pulseaudio-alsa pamixer
 sudo pacman -S --noconfirm xclip scrot
 
 ## install font
-sudo pacman -S --noconfirm ttf-dejavu otf-ipafont ttf-hanazono
+sudo pacman -S --noconfirm ttf-dejavu noto-fonts-emoji otf-ipafont ttf-hanazono
 
 ## install dmenu
 sudo pacman -S --noconfirm dmenu
@@ -92,26 +84,15 @@ sudo pacman -S --noconfirm nautilus
 
 ## fcitx
 sudo pacman -S --noconfirm fcitx fcitx-im fcitx-unikey fcitx-configtool fcitx-mozc
-
-echo 'GTK_IM_MODULE=fcitx' >> ~/.pam_environment
-echo 'QT_IM_MODULE=fcitx' >> ~/.pam_environment
-echo 'XMODIFIERS=@im=fcitx' >> ~/.pam_environment
-
-## install pikaur (aur helper)
-git clone https://aur.archlinux.org/pikaur.git ~/pikaur
-cd ~/pikaur
-sudo pacman -S --noconfirm pyalpm python
-makepkg
-sudo pacman -U --noconfirm *.pkg.tar.xz
-cd --
-rm -rf ~/pikaur
-
-## install i3lock-next
-pikaur -S --noconfirm i3lock-next-git
+cp /configure/Arch/config_file/pam_environment ~/.pam_environment
 
 ## time zone
 sudo timedatectl set-timezone Asia/Ho_Chi_Minh
 sudo timedatectl set-local-rtc 1
+
+## compton
+sudo pacman -S --noconfirm compton
+cp /configure/Arch/config_file/compton ~/.config/compton.conf
 EOF
 
 usermod -s /bin/zsh $username # change default shell for user $username
