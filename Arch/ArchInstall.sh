e88a63d5f90c18c85fff0163e9d1422efbdaf9a9
nobabykill <linh1612340@gmail.com>
2020-06-05 22:37:51 +0700

    change host name

--- Arch/ArchInstall.sh
+++ Arch/ArchInstall.sh
@@ -23,7 +23,7 @@ locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
 ## hostname
-echo "ArchLinux" > /etc/hostname
+echo "Arch" > /etc/hostname
 
 ## install boot loader
 bootctl --path=/boot install
