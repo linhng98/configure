c06f1ff55cd8649bb74784974133e94868adc8a5
nobabykill <linh1612340@gmail.com>
2020-06-26 17:25:13 +0700

    use mono font nerd

--- Arch/config_file/st.config.h
+++ Arch/config_file/st.config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVuSansMono Nerd Font:pixelsize=20:antialias=true:autohint=true";
+static char *font = "DejaVuSansMono Nerd Font Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
